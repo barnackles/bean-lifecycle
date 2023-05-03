8f4d215b8e4072676307cd5e8a27dda46bac2ff1
barnackles <p.martewicz@yahoo.com>
2023-05-03 16:00:52 +0200

    Adding print in BeanLifecycleApplication class.

--- src/main/java/com/kodilla/lifecycle/AppConfiguration.java
+++ src/main/java/com/kodilla/lifecycle/AppConfiguration.java
@@ -1,4 +1,4 @@
-package com.kodilla.beanlifecycle;
+package com.kodilla.lifecycle;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/kodilla/lifecycle/BeanLifecycleApplication.java
+++ src/main/java/com/kodilla/lifecycle/BeanLifecycleApplication.java
@@ -1,13 +1,18 @@
-package com.kodilla.beanlifecycle;
+package com.kodilla.lifecycle;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BeanLifecycleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeanLifecycleApplication.class, args);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(AppConfiguration.class);
+		context.refresh();
+
+		LibraryManager manager = context.getBean(LibraryManager.class);
+		System.out.println("Context and beans are set up and ready to work");
+		context.close();
 	}
 
 }
